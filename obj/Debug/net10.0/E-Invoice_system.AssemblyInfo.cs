76290eccf2dd2ba97b13aaa1235087e84aee2ac4
Ashir Ali <ashir987ali@gmail.com>
2026-01-29 16:57:24 -0800

    Login design changes

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae30c0cc1e4fa96783279d7ffc7825368f8f23d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6963d24bd3e51bde7dfe44af001fbde059c42aa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
