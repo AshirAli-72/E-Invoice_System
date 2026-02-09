09a2a9363ad6dcb59678ca2d6dfb0b91f6921314
Ashir Ali <ashir987ali@gmail.com>
2026-02-09 15:49:26 -0800

    sale screens changes

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8906aa56fe1f0f95311fe20e08cd8b8cc20a3fa5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a84a46eb362cd26cd1e33aed0482d6765b436e99")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
