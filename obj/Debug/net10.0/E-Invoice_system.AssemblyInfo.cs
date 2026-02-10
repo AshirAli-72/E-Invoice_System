050dc68e97d73db417e20952b6fcb8962468578e
Ashir Ali <ashir987ali@gmail.com>
2026-02-09 17:11:46 -0800

    work on responsive

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5734ec6295ef6793ce14451d04c5ad2b6b5b6c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e43b5249d89da787ed2722951513f305caa6827f")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
