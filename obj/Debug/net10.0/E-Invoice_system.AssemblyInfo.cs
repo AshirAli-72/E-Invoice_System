17add5fbca1cd116b2d35d2d52cedcd0532166d3
Ashir Ali <ashir987ali@gmail.com>
2026-02-19 16:51:35 -0800

    responsive sale screen

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+afd2609186eb0338bcdbc1160a7bbfeafd4752e1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c893211d91f9efac8196a8a3621fe9e242ea9b66")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
