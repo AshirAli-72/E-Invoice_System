f6936f509cf1a616d360ca7a6cfdfd324f63aec2
Ashir Ali <ashir987ali@gmail.com>
2026-02-25 15:46:58 +0500

    new changes in sale

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88dc3e1a624f1571de03c7e6f0cece0b9a8d0235")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae4bc52e3016828667060de6ea63a8ecb97775e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
