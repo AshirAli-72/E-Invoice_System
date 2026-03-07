7356bdb767b2ea4419334b98c3548621c4a9e0bd
Ashir Ali <ashir987ali@gmail.com>
2026-03-07 15:21:19 +0500

    invoice design changes

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+479233fd402929cbc4b4decb8af69255f1870761")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+48aaa6070bf306af371aba932a09899177481ee1")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
