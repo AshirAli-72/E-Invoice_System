76c968766e21f16c8a348070c192e6ce9f8aa741
Ashir Ali <ashir987ali@gmail.com>
2026-03-09 14:56:31 +0500

    invoice layout changes

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc70941855cd5d4bed4046d2acbc79fda535d391")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f14204586c0549f9c8f4db1ccaac218175284b1")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
