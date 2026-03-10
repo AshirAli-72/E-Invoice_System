485925b7a56585c7f2122e7f40065c745334c712
Ashir Ali <ashir987ali@gmail.com>
2026-03-10 15:10:17 +0500

    receipt changes

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ba4819713b271e7b44d6f3084246fab45a8ab929")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b21ee4cabf4b36723d5093157f19d9864b1fe1b")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
