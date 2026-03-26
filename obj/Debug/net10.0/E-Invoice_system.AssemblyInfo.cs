d792bbf40d87cfc67a4f5840143a9e9c28a5ab8d
Ashir Ali <ashir987ali@gmail.com>
2026-03-26 12:17:30 +0500

    convert the whole app in razor and blazor

--- obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
+++ obj/Debug/net10.0/E-Invoice_system.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+899eeee010b318274c7aa5f81833b114360c518a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e16a65e203a8d4872d48f18f59f13be0fedc4448")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-Invoice_system")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
